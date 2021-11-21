60d0469ed03e05814ba9d7ba323be7670e40fd4c
sangholee990 <tkdgh6427@naver.com>
2021-11-21 22:03:30 +0900

    hello

--- Project/Login.java
+++ Project/Login.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-// pull test
+// pushtest!!
 public class Login {
 
 	public int login(String ID, String PASSWD) throws SQLException {
