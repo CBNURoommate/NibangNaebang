ecbab3670e25e5ec6216074da4f923154aff4da4
DohyunJegal <dohyunm14@chungbuk.ac.kr>
2021-11-22 01:26:35 +0900

    .

--- Project/Login.java
+++ Project/Login.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-// pushtest!! 33 풀용
+// pushtest!! 33 풀용121
 public class Login {
 
 	public int login(String ID, String PASSWD) throws SQLException {
