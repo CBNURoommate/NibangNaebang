bc57daf721acc6ef4935bec42d1c45a7d32c6564
tkdgh990612 <tkdgh990612@naver.com>
2021-11-25 04:03:38 +0900

    weq

--- Project/empty.java
+++ Project/empty.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//荑쇰━�뿰�뒿? ���슜  commit checksss
+//荑쇰━�뿰�뒿? ���슜  commit checksssas
 public class empty{
 	
 	
