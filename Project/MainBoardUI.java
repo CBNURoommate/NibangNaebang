2ab2621949b5a195aedaa73c2da9056c2d98148d
hm5961 <71277818+hm5961@users.noreply.github.com>
2021-11-26 00:33:53 +0900

    Add files via upload

--- Project/MainBoardUI.java
+++ Project/MainBoardUI.java
@@ -1,179 +1,88 @@
 package Project;
 
-import javax.swing.*;// 메인보드 ui
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
 
-public class MainBoardUI extends JFrame{
-    JFrame jframe = new JFrame();
-    JPanel jpanel = new JPanel();
-    JTextField t1 = new JTextField();
-    JTextField t2 = new JTextField();
-    JTextField t3 = new JTextField();
-    JTextField t4 = new JTextField();
-    JTextArea ta = new JTextArea();
-    JButton btn1, btn2, btn3, btn4, btn5;
-    JLabel ㅣ1 = new JLabel("이름 : ");
-    JLabel ㅣ2 = new JLabel("생년월일 : ");
-    JLabel ㅣ3 = new JLabel("전화번호 : ");
-    JLabel ㅣ4 = new JLabel("검색내용 : ");
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
 
-    MainBoardUI() {
-        GUI_init();
-    }
+public class MainBoardUI extends JFrame {
 
-    public void GUI_init() {
-        jframe.setTitle("회원관리");
-        jframe.setBounds(50, 50, 480, 450);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setVisible(true);
-        jpanel.setLayout(null);
-        jframe.add(jpanel);
+	private JPanel contentPane;
 
-        t1.setBounds(75, 25, 70, 25);
-        jpanel.add(t1);
-        ㅣ1.setBounds(37, 21, 70, 30);
-        jpanel.add(ㅣ1);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainBoardUI frame = new MainBoardUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-        t2.setBounds(213, 25, 70, 25);
-        jpanel.add(t2);
-        ㅣ2.setBounds(150, 21, 70, 30);
-        jpanel.add(ㅣ2);
-
-        t3.setBounds(352, 25, 80, 25);
-        jpanel.add(t3);
-        ㅣ3.setBounds(290, 21, 70, 30);
-        jpanel.add(ㅣ3);
-
-        t4.setBounds(213, 105, 80, 25);
-        jpanel.add(t4);
-        ㅣ4.setBounds(150, 100, 70, 30);
-        jpanel.add(ㅣ4);
-
-        JScrollPane jsp = new JScrollPane(ta);
-        jsp.setBounds(23, 145, 420, 250);
-        jpanel.add(jsp);
-
-        jpanel.add(btn1 = new JButton("입력"));
-        btn1.setBounds(40, 60, 80, 30);
-
-        jpanel.add(btn2 = new JButton("출력"));
-        btn2.setBounds(145, 60, 80, 30);
-
-        jpanel.add(btn3 = new JButton("수정"));
-        btn3.setBounds(250, 60, 80, 30);
-
-        jpanel.add(btn4 = new JButton("삭제"));
-        btn4.setBounds(350, 60, 80, 30);
-
-        jpanel.add(btn5 = new JButton("검색"));
-        btn5.setBounds(300, 100, 80, 30);
-
-        Controller dao = new Controller();
-
-
-        // 회원 추가
-        btn1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                ta.setText("");
-
-                String name = t1.getText();
-                String birth = t2.getText();
-                String tel = t3.getText();
-
-                dao.insertMember(new Model(name, birth, tel));
-
-                ta.append("입력 완료 \n");
-
-                t1.setText("");
-                t2.setText("");
-                t3.setText("");
-                t4.setText("");
-            }
-        });
-
-        // 회원 목록 출력
-        btn2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                ta.setText("");
-                ArrayList<Model> arr = new ArrayList<Model>();
-                arr = dao.readMember();
-
-                ta.append("\t" + "name" + "\t" + "birth" + "\t" + "tel\n");
-                ta.append("\t" + "------------------------------------------------------------\n");
-
-                for (int i = 0; i < arr.size(); i++) {
-                    ta.append("\t" + arr.get(i).getName() + " \t " + arr.get(i).getBirth() + " \t " + arr.get(i).getTel()
-                            + "\n");
-                }
-            }
-        });
-
-        // 회원 수정
-        btn3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                ta.setText("");
-
-                String name = t1.getText();
-                String birth = t2.getText();
-                String tel = t3.getText();
-
-                dao.updateMember(name, tel);
-                ta.append("수정 완료 \n");
-
-                t1.setText("");
-                t2.setText("");
-                t3.setText("");
-                t4.setText("");
-            }
-        });
-
-        // 회원 삭제
-        btn4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                ta.setText("");
-
-                String name = t1.getText();
-                dao.deleteMember(name);
-                ta.append("삭제 완료 \n");
-
-                t1.setText("");
-                t2.setText("");
-                t3.setText("");
-                t4.setText("");
-            }
-        });
-
-        // 회원 검색
-        btn5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                ta.setText("");
-                String content = t4.getText();
-
-                ArrayList<Model> arr = new ArrayList<Model>();
-                arr = dao.searchMember(content);
-                ta.append(" \n");
-
-                ta.append("\t" + "name" + "\t" + "birth" + "\t" + "tel\n");
-                ta.append("\t" + "------------------------------------------------------------\n");
-
-                for (int i = 0; i < arr.size(); i++) {
-                    ta.append("\t" + arr.get(i).getName() + " \t " + arr.get(i).getBirth() + " \t " + arr.get(i).getTel()
-                            + "\n");
-                }
-
-                t1.setText("");
-                t2.setText("");
-                t3.setText("");
-                t4.setText("");
-            }
-        });
-    }
+	/**
+	 * Create the frame.
+	 */
+	public MainBoardUI() {
+		setTitle("MainBoard");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1600, 900);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel mainLabel = new JLabel("\uD604\uC7AC \uC0AC\uC6A9\uC790 \uBAA9\uB85D");
+		mainLabel.setFont(new Font("나눔고딕", Font.BOLD, 50));
+		mainLabel.setBounds(707, 178, 460, 70);
+		
+		JTextPane textPane = new JTextPane();
+		
+		JList UserList = new JList();
+		
+		JRadioButton SmokeFilterButton = new JRadioButton("\uD761\uC5F0\uC790 \uAC70\uB974\uAE30");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(textPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(46)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(mainLabel, GroupLayout.PREFERRED_SIZE, 398, GroupLayout.PREFERRED_SIZE)
+						.addComponent(SmokeFilterButton)
+						.addComponent(UserList, GroupLayout.PREFERRED_SIZE, 1128, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(393, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(58)
+					.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 793, GroupLayout.PREFERRED_SIZE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(mainLabel)
+					.addGap(18)
+					.addComponent(SmokeFilterButton)
+					.addGap(19)
+					.addComponent(UserList, GroupLayout.PREFERRED_SIZE, 376, GroupLayout.PREFERRED_SIZE)
+					.addGap(39))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
 }
-
