ed96be96d64b92540fa374d78cae71fd04585d9d
sangholee990 <80809068+sangholee990@users.noreply.github.com>
2021-11-26 16:18:10 +0900

    Update Model.java

--- Project/Model.java
+++ Project/Model.java
@@ -4,11 +4,21 @@ public class Model {
     String name;
     String birth;
     String tel;
+    int a;
+    int b;
+    int c;
+    int d;
+    int num;
+    
 
-    public Model(String name, String birth, String tel) {
+    public Model(String name, String birth, String tel, int a, int b, int c, int d) {
         this.birth = birth;
         this.name = name;
         this.tel = tel;
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.d=d;
     }
 
     public String getName() {
@@ -22,6 +32,22 @@ public class Model {
     public String getTel() {
         return tel;
     }
+    
+    public int getA() {
+        return a;
+    }
+    
+    public int getB() {
+        return b;
+    }
+    
+    public int getC() {
+        return c;
+    }
+    
+    public int getD() {
+        return d;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -33,5 +59,21 @@ public class Model {
 
     public void setTel(String tel) {
         this.tel = tel;
+    }
+    
+    public void setA(int num) {
+        this.a = num;
+    }
+    
+    public void setB(int num) {
+        this.b = num;
+    }
+    
+    public void setC(int num) {
+        this.c = num;
+    }
+    
+    public void setD(int num) {
+        this.c = num;
     }
 }
