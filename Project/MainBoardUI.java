f31fa02ebe8b2d134214d6eeb561651bce329190
sangholee990 <80809068+sangholee990@users.noreply.github.com>
2021-12-04 02:58:45 +0900

    Update MainBoardUI.java

--- Project/MainBoardUI.java
+++ Project/MainBoardUI.java
@@ -48,10 +48,10 @@ public class MainBoardUI {
     private final JComboBox coldBox = new JComboBox();
     private final JComboBox sleepBox = new JComboBox();
     private final JComboBox wakeBox = new JComboBox();
-    private final JComboBox whenshowerBox = new JComboBox();
+    private final JComboBox showerBox = new JComboBox();
     private final JComboBox callBox = new JComboBox();
     private final JComboBox eatBox = new JComboBox();
-    private final JComboBox comboBox_2 = new JComboBox();
+    private final JComboBox soolBox = new JComboBox();
 
     MainBoardUI() {
         GUI_init();
@@ -194,16 +194,16 @@ public class MainBoardUI {
         sleepBox.setBounds(1062, 342, 111, 25);
         
         jpanel.add(sleepBox);
-        wakeBox.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "7", "8", "9", "10", "11", "\uC624\uD6C4"}));
+        wakeBox.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "7\uC2DC", "8\uC2DC", "9\uC2DC", "10\uC2DC", "11\uC2DC", "\uC624\uD6C4"}));
         wakeBox.setFont(new Font("나눔고딕", Font.BOLD, 12));
         wakeBox.setBounds(1061, 391, 112, 25);
         
         jpanel.add(wakeBox);
-        whenshowerBox.setFont(new Font("나눔고딕", Font.BOLD, 12));
-        whenshowerBox.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "\uC544\uCE68", "\uC800\uB141", "\uC720\uB3D9\uC801"}));
-        whenshowerBox.setBounds(1062, 436, 111, 25);
+        showerBox.setFont(new Font("나눔고딕", Font.BOLD, 12));
+        showerBox.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "\uC544\uCE68", "\uC800\uB141", "\uC720\uB3D9\uC801"}));
+        showerBox.setBounds(1062, 436, 111, 25);
         
-        jpanel.add(whenshowerBox);
+        jpanel.add(showerBox);
         callBox.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "\uBC18\uB300", "\uC0C1\uAD00\uC5C6\uC74C", "\uCC2C\uC131\t", "\uAC04\uB2E8\uD55C\uC6A9\uBB34"}));
         callBox.setFont(new Font("나눔고딕", Font.BOLD, 12));
         callBox.setBounds(1062, 480, 111, 25);
@@ -214,11 +214,11 @@ public class MainBoardUI {
         eatBox.setBounds(1062, 527, 111, 25);
         
         jpanel.add(eatBox);
-        comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "\uC548\uBA39\uC74C", "\uAC00\uB054\uBA39\uC74C", "\uC790\uC8FC\uBA39\uC74C"}));
-        comboBox_2.setFont(new Font("나눔고딕", Font.BOLD, 12));
-        comboBox_2.setBounds(1062, 572, 111, 23);
+        soolBox.setModel(new DefaultComboBoxModel(new String[] {"\uC804\uCCB4", "\uC548\uBA39\uC74C", "\uAC00\uB054\uBA39\uC74C", "\uC790\uC8FC\uBA39\uC74C"}));
+        soolBox.setFont(new Font("나눔고딕", Font.BOLD, 12));
+        soolBox.setBounds(1062, 572, 111, 23);
         
-        jpanel.add(comboBox_2);
+        jpanel.add(soolBox);
 
         
         scrollPane.setVisible(true); 
@@ -237,18 +237,26 @@ public class MainBoardUI {
                 ArrayList<Model> arr = new ArrayList<Model>();
                 arr = dao.readMember();
 
-                ta.append( "id" + "\t" + "이름" + "\t" + "성별"+ "\t" + "기숙사"+ "\t" + "학번"+ "\t" + "나이"+ "\t" + "흡연여부"+ "\t" + "코골이"+ "\t" + "단과대\n");
-                ta.append( "------------------------------------------------------------------------------------------------------------------------------------"
-                		+ "-----------------------------------------------------\n");
+                ta.append( "id" + "\t" + "이름" + "\t" + "성별"+ "\t" + "기숙사"+ "\t" + "학번"+ "\t" + "나이"+ "\t" + "흡연여부"+ "\t" + "코골이" + "\t" + "단과대"+ "\t" + "더위"+ "\t" + "추위"+ "\t" + "취침시간"+ "\t" + "기상시간"+ "\t" + "샤워시간"+ "\t" + "실내통화"+ "\t" + "실내취식"+ "\t" + "술\t\n");
+                ta.append( "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+                		+ "-------------------------------------------------------------------------------------------------------------------\n");
          
                 
                 String gender_a="a";
                 String dom_a="a";
                 String smoking_a="a";
                 String jam_a="a";
-                String dpt_a="a";
                 String hakbun_a ="a";
                 String age_a ="a";
+                String heat_a="a";
+                String cold_a="a";
+                String sleep_a="a";
+                String wake_a="a";
+                String shower_a="a";
+                String call_a="a";
+                String eat_a="a";
+                String sool_a="a";
+                
 
                 
                 switch(CurrentUser.getGender()) {
@@ -339,15 +347,106 @@ public class MainBoardUI {
                     	 age_a = arr.get(i).age;
                      
                      
-                    if(dptbox.getSelectedItem() == "전체") {
-                        if(arr.get(i).getGender() == gender_a && arr.get(i).getDom() == dom_a && arr.get(i).getSmoking() == smoking_a && arr.get(i).getJam() == jam_a && arr.get(i).getHakbun() == hakbun_a && arr.get(i).getAge() == age_a)
+                     if(heatBox.getSelectedItem() == "적게탐")
+                    	 heat_a = "적게탐";
+                     else if(heatBox.getSelectedItem() == "보통")
+                    	 heat_a = "보통";
+                     else if(heatBox.getSelectedItem() == "많이탐")
+                    	 heat_a = "많이탐";
+                     else if(heatBox.getSelectedItem() == "전체")
+                    	 heat_a = arr.get(i).heat;
+                     
+                     if(coldBox.getSelectedItem() == "적게탐")
+                    	 cold_a = "적게탐";
+                     else if(heatBox.getSelectedItem() == "보통")
+                    	 cold_a = "보통";
+                     else if(heatBox.getSelectedItem() == "많이탐")
+                    	 cold_a = "많이탐";
+                     else if(heatBox.getSelectedItem() == "전체")
+                    	 cold_a = arr.get(i).cold;
+                    
+                     if(sleepBox.getSelectedItem() == "22시")
+                    	 sleep_a = "22시";
+                     else if(sleepBox.getSelectedItem() == "23시")
+                    	 sleep_a = "23시";
+                     else if(sleepBox.getSelectedItem() == "24시")
+                    	 sleep_a = "23시";
+                     else if(sleepBox.getSelectedItem() == "0시")
+                    	 sleep_a = "0시";
+                     else if(sleepBox.getSelectedItem() == "1시")
+                    	 sleep_a = "1시";
+                     else if(sleepBox.getSelectedItem() == "2시")
+                    	 sleep_a = "2시";
+                     else if(sleepBox.getSelectedItem() == "3시")
+                    	 sleep_a = "3시";
+                     else if(sleepBox.getSelectedItem() == "전체")
+                    	 sleep_a = arr.get(i).sleep;
+                     
+                     if(wakeBox.getSelectedItem() == "7시")
+                    	 wake_a = "7시";
+                     else if(wakeBox.getSelectedItem() == "8시")
+                    	 wake_a = "8시";
+                     else if(wakeBox.getSelectedItem() == "9시")
+                    	 wake_a = "9시";
+                     else if(wakeBox.getSelectedItem() == "10시")
+                    	 wake_a = "10시";
+                     else if(wakeBox.getSelectedItem() == "11시")
+                    	 wake_a = "11시";
+                     else if(wakeBox.getSelectedItem() == "오후")
+                    	 wake_a = "오후";
+                     else if(wakeBox.getSelectedItem() == "전체")
+                    	 wake_a = arr.get(i).wake;
+                     
+                     if(showerBox.getSelectedItem() == "아침")
+                    	 shower_a = "아침";
+                     else if(showerBox.getSelectedItem() == "저녁")
+                    	 shower_a = "저녁";
+                     else if(showerBox.getSelectedItem() == "유동적")
+                    	 shower_a = "유동적";
+                     else if(showerBox.getSelectedItem() == "전체")
+                    	 shower_a = arr.get(i).shower;
+                     
+                     if(callBox.getSelectedItem() == "반대")
+                    	 call_a = "반대";
+                     else if(callBox.getSelectedItem() == "상관없음")
+                    	 call_a = "상관없음";
+                     else if(callBox.getSelectedItem() == "찬성")
+                    	 call_a = "찬성";
+                     else if(callBox.getSelectedItem() == "간단한용무")
+                    	 call_a = "간단한용무";
+                     else if(callBox.getSelectedItem() == "전체")
+                    	 call_a = arr.get(i).call;
+                     
+                     if(eatBox.getSelectedItem() == "불가")
+                    	 eat_a = "불가";
+                     else if(eatBox.getSelectedItem() == "냄새x")
+                    	 eat_a = "냄새x";
+                     else if(eatBox.getSelectedItem() == "가능")
+                    	 eat_a = "가능";
+                     else if(eatBox.getSelectedItem() == "전체")
+                    	 eat_a = arr.get(i).eat;
+                     
+                     if(soolBox.getSelectedItem() == "안먹음")
+                    	 sool_a = "안먹음";
+                     else if(soolBox.getSelectedItem() == "가끔먹음")
+                    	 sool_a = "가끔먹음";
+                     else if(soolBox.getSelectedItem() == "자주먹음")
+                    	 sool_a = "자주먹음";
+                     else if(soolBox.getSelectedItem() == "전체")
+                    	 sool_a = arr.get(i).sool;
+                     
+                     
+                     
+                     if(dptbox.getSelectedItem() == "전체") {
+                        if(arr.get(i).getGender() == gender_a && arr.get(i).getDom() == dom_a && arr.get(i).getSmoking() == smoking_a && arr.get(i).getJam() == jam_a && arr.get(i).getHakbun() == hakbun_a && arr.get(i).getAge() == age_a && arr.get(i).getHeat() == heat_a && arr.get(i).getCold() == cold_a && arr.get(i).getSleep() == sleep_a && arr.get(i).getWake() == wake_a && arr.get(i).getShower() == shower_a && arr.get(i).getCall() == call_a  && arr.get(i).getEat() == eat_a && arr.get(i).getSool() == sool_a)
             		    	 ta.append( arr.get(i).getID() + " \t " + arr.get(i).getName() + " \t " + arr.get(i).getGender()+ "\t " + arr.get(i).getDom()
-                		     + " \t " + arr.get(i).getHakbun() + " \t " + arr.get(i).getAge() + " \t " + arr.get(i).getSmoking()  + " \t " + arr.get(i).getJam() + " \t " + arr.get(i).getDpt()+ "\n");
-                    }
-                    else
-                    	if(arr.get(i).getGender() == gender_a && arr.get(i).getDom() == dom_a && arr.get(i).getSmoking() == smoking_a && arr.get(i).getJam() == jam_a && arr.get(i).getDpt() == dptbox.getSelectedItem() && arr.get(i).getHakbun() == hakbun_a && arr.get(i).getAge() == age_a)
+                		     + " \t " + arr.get(i).getHakbun() + " \t " + arr.get(i).getAge() + " \t " + arr.get(i).getSmoking()  + " \t " + arr.get(i).getJam() + " \t " + arr.get(i).getDpt()+ " \t " + arr.get(i).getHeat() + " \t " + arr.get(i).getCold() + " \t " + arr.get(i).getSleep()  + " \t " + arr.get(i).getWake() +  " \t " + arr.get(i).getShower() + " \t " + arr.get(i).getCall() + " \t " + arr.get(i).getEat() + " \t " + arr.get(i).getSool() +"\n"  );
+                     }
+                    
+                     else
+                    	if(arr.get(i).getGender() == gender_a && arr.get(i).getDom() == dom_a && arr.get(i).getSmoking() == smoking_a && arr.get(i).getJam() == jam_a && arr.get(i).getDpt() == dptbox.getSelectedItem() && arr.get(i).getHakbun() == hakbun_a && arr.get(i).getAge() == age_a && arr.get(i).getHeat() == heat_a  && arr.get(i).getCold() == cold_a && arr.get(i).getSleep() == sleep_a && arr.get(i).getWake() == wake_a && arr.get(i).getShower() == shower_a && arr.get(i).getCall() == call_a && arr.get(i).getEat() == eat_a && arr.get(i).getSool() == sool_a)
            		    	   ta.append( arr.get(i).getID() + " \t " + arr.get(i).getName() + " \t " + arr.get(i).getGender()+ "\t " + arr.get(i).getDom()
-               		        + " \t " + arr.get(i).getHakbun() + " \t " + arr.get(i).getAge() + " \t " + arr.get(i).getSmoking()  + " \t " + arr.get(i).getJam() + " \t " + arr.get(i).getDpt()+ "\n");
+               		        + " \t " + arr.get(i).getHakbun() + " \t " + arr.get(i).getAge() + " \t " + arr.get(i).getSmoking()  + " \t " + arr.get(i).getJam() + " \t " + arr.get(i).getDpt()+" \t " + arr.get(i).getHeat() + " \t " + arr.get(i).getCold() + " \t " + arr.get(i).getSleep() + " \t " + arr.get(i).getWake()+  " \t " + arr.get(i).getShower()+" \t " + arr.get(i).getCall() + " \t " + arr.get(i).getEat() + " \t " + arr.get(i).getSool() + "\n");
                 	               		    
            		    
            		              		   
@@ -369,9 +468,9 @@ public class MainBoardUI {
                 arr = dao.searchMember(content);
                 ta.append("\n");
 
-                ta.append( "id" + "\t" + "이름" + "\t" + "성별"+ "\t" + "기숙사"+ "\t" + "학번"+ "\t" + "나이"+ "\t" + "흡연여부"+ "\t" + "코골이" + "\t" + "단과대\n");
+                ta.append( "id" + "\t" + "이름" + "\t" + "성별"+ "\t" + "기숙사"+ "\t" + "학번"+ "\t" + "나이"+ "\t" + "흡연여부"+ "\t" + "코골이" + "\t" + "단과대"+ "\t" + "더위"+ "\t" + "추위"+ "\t" + "취침시간"+ "\t" + "기상시간"+ "\t" + "샤워시간"+ "\t" + "실내통화"+ "\t" + "실내취식"+ "\t" + "술\n");
                 ta.append( "-------------------------------------------------------------------------------------------------------------------------------------"
-                		+ "-----------------------------------------------------\n");
+                		+ "----------------------------------------------------------------------------------------------------\n");
 
                 for (int i = 0; i < arr.size(); i++) {
                 	ta.append(arr.get(i).getID() + " \t " + arr.get(i).getName() + " \t " + arr.get(i).getGender()+ "\t " + arr.get(i).getDom()
